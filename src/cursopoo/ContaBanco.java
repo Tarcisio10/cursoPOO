6a89f5e846f8b386c2d83c3f63cab510fc4df7fb
Tarcisio Blois <tarcisio.b.neto@gmail.com>
2021-04-26 16:09:35 -0300

    "Review e retomada de conteudo antes do conceito de encapsulamento"

---
+++ src/cursopoo/ContaBanco.java
@@ -0,0 +1,131 @@
+package cursopoo;
+
+public class ContaBanco {
+
+    public int numConta;
+    protected String tipoConta;
+    private String dono;
+    private float saldo;
+    private boolean status;
+
+    public ContaBanco(String tC) {
+        this.setStatus(true);
+        //setTipoConta(tC);
+        //abrirConta(tp);
+        this.setTipoConta(tC);
+        System.out.println("Status:" + this.status);
+        System.out.println("TIPO DE CONTA:" + this.tipoConta);
+        System.out.println("NUMERO DA CONTA:" + getNumConta());
+        System.out.println("SALDO: " + getSaldo());
+    }
+
+    //Métodos personalizados
+    public void abrirConta(String tp) {
+        this.setStatus(true);
+        if ("cc".equals(tp)) {
+            this.setSaldo(50);
+        } else if ("cf".equals(tp)) {
+            this.setSaldo(150);
+        }
+
+    }
+
+    public void fecharConta() { //Não mostrar coisas em métodos, encapsular.
+        if (this.getSaldo() > 0) {
+            System.out.println("Nao posso fechar!! Saldo positivo");
+        } else if (this.getSaldo() < 0) {
+            System.out.println("Não posso fechar!! Saldo negativo");
+        } else {
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso!!");
+        }
+    }
+
+    public void depositar(boolean sts, float v) {
+        if (sts == true) {
+            //v += this.saldo;
+            this.setSaldo(this.getSaldo() + v);
+            System.out.println("Deposito realizado");
+        } else {
+            System.out.println("Imposivel depositar");
+        }
+    }
+
+    public void sacar(int v) {
+        if (this.getStatus()) {
+            if (this.getSaldo() >= v) {
+                this.setSaldo(this.getSaldo() - v);
+                System.out.println("SAQUE REALIZADO");
+            } else {
+                System.out.println("SALDO INSULFICIENTE PARA SAQUE");
+            }
+        } else {
+            System.out.println("CONTA INATIVA");
+        }
+    }
+
+    public void pagarMensal() {
+        int v = 0;
+        if ("CC".equals(this.getTipoConta())) {
+            v = 12;
+        } else if ("CF".equals(this.getTipoConta())) {
+            v = 20;
+        }
+        if (this.getStatus()) {
+            this.setSaldo(this.getSaldo() - v);
+            System.out.println("Mensalidade paga");
+        }else{
+            System.out.println("");
+        }
+    }
+
+    //Métodos especiais
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+    public String setTipoConta(String tipoConta) {
+        this.tipoConta = tipoConta;
+        if ("cc".equals(this.tipoConta)) {
+            //this.tipoConta = tp;
+            this.setSaldo(50.2f);
+        } else if ("cf".equals(tipoConta)) {
+            //this.tipoConta = tp;
+            this.setSaldo(150);
+        }
+        return this.tipoConta;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    public boolean getStatus() {
+        return this.status;
+    }
+
+}
--- src/cursopoo/CursoPOO.java
+++ src/cursopoo/CursoPOO.java
@@ -5,8 +5,13 @@ public class CursoPOO {
     public static void main(String[] args) {
 
         //Caneta c2 = new Caneta("TOP", 1.0f, "azul");
-        Banco b1 = new Banco("cc");
+        ContaBanco b1 = new ContaBanco("cf");
+        b1.setDono("Tarsa");
+        b1.setNumConta(111);
+        b1.getSaldo();
         //b1.getSaldo();
+        //b1.fecharConta(this.getStatus());
+        //b1.fecharConta();
     }
 
 }
