2406f1c043e546385f7c05402a71577fbb540b6d
Tarcisio Blois <tarcisio.b.neto@gmail.com>
2021-04-28 20:31:17 -0300

    Encapsulamento-claseInterface

---
+++ src/cursopoo/Q1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cursopoo;
+
+/**
+ *
+ * @author tbn
+ */
+public class Q1 {
+    public static void main(String[] args) {
+        int i = 2;
+        if (i++ <= 3 && ++i > 4) {
+            i=3;
+            
+        }
+        System.out.println(i);
+    }
+}
---
+++ src/encapsulamento/Controle.java
@@ -0,0 +1,10 @@
+package encapsulamento;
+
+public class Controle {
+    public static void main(String[] args) {
+        ControleRemoto  c = new ControleRemoto();
+        c.ligar();
+        c.abrirMenu();
+    }
+    
+}
